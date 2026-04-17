fbbb781e2076e7a01a9d80daf5ebe0ee881c3ff9
nidhish <72427739+nidhishgajjar@users.noreply.github.com>
2026-04-17 13:21:52 +0530

    Update configctl/loaders/__init__.py

--- configctl/loaders/__init__.py
+++ configctl/loaders/__init__.py
@@ -1,9 +1,12 @@
 from configctl.loaders import json_loader
+from configctl.loaders import yaml_loader
 from configctl.errors import ConfigError
 
 
 _LOADERS = {
     ".json": json_loader.load,
+    ".yaml": yaml_loader.load,
+    ".yml":  yaml_loader.load,
 }
 
 
